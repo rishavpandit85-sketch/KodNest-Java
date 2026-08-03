4587d2cfc2f0dd33ef5203c0a3ff75843b4fd994
RISHAV PANDIT <rishavpandit85@gmail.com>
2026-08-03 20:20:13 +0530

    changes

--- track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/WelcomeToJava.java
+++ track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/WelcomeToJava.java
@@ -3,5 +3,6 @@ public class WelcomeToJava {
 
     public static void main(String[] args) {
         System.out.println("Welcome to Java Track");
+        System.out.println("I already know to build programming Logic");
     }
 }
