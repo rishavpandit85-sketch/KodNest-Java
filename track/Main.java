5b4a71735cc58d9adb97812c4c7deba87c3d69cb
RISHAV PANDIT <rishavpandit85@gmail.com>
2026-08-03 20:41:21 +0530

    changes

---
+++ track/Main.java
@@ -0,0 +1,9 @@
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Java: 10");
+        System.out.println("Aptitude: 5");
+        System.out.println("Total: 15");
+    }
+}
