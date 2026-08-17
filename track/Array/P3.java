5bd87f21601bca35d0f3ef6dfb69e91c258702a5
RISHAV PANDIT <rishavpandit85@gmail.com>
2026-08-17 15:42:11 +0530

    Added array programs

---
+++ track/Array/P3.java
@@ -0,0 +1,24 @@
+
+import java.util.Scanner;
+
+public class P3 {
+
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+
+        int a[] = new int[5];
+
+        System.out.println("Enter Array Elements:");
+
+        for (int i = 0; i <= a.length - 1; i++) {
+            a[i] = scan.nextInt();
+        }
+
+        System.out.println("Array Elements are:");
+
+        for (int i = 0; i <= a.length - 1; i++) {
+            System.out.println(a[i]);
+        }
+    }
+}
---
+++ track/Array/P4.java
@@ -0,0 +1,12 @@
+
+public class P4 {
+
+    public static void main(String[] args) {
+
+        int a[] = {10, 20, 30, 40, 50};
+
+        for (int x : a) {
+            System.out.println(x);
+        }
+    }
+}
