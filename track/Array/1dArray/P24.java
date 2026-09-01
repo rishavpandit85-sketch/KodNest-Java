d56de67c784e4150666d9273a6b37ded3a11fe5d
RISHAV PANDIT <rishavpandit85@gmail.com>
2026-09-01 11:15:26 +0530

    Added Java array programs

---
+++ track/Array/1dArray/P24.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+public class P24 {
+
+    public static void main(String[] args) {
+
+        try (Scanner scanner = new Scanner(System.in)) {
+            int n = scanner.nextInt();
+
+            int[] a = new int[n];
+
+            for (int i = 0; i < n; i++) {
+                a[i] = scanner.nextInt();
+            }
+
+            int write = 0;
+
+            // Move all non-zero elements to the front
+            for (int read = 0; read < n; read++) {
+
+                if (a[read] != 0) {
+                    a[write] = a[read];
+                    write++;
+                }
+            }
+
+            // Fill remaining positions with zeros
+            while (write < n) {
+                a[write] = 0;
+                write++;
+            }
+
+            // Print the same array
+            for (int i = 0; i < n; i++) {
+                System.out.print(a[i] + " ");
+            }
+        }
+    }
+}
