1ccf0f9a76ccd37c0088383b3b6d648c303a6788
RISHAV PANDIT <rishavpandit85@gmail.com>
2026-09-11 19:02:57 +0530

    Added to Java String Practice Problems

---
+++ track/String/Palindrome1.java
@@ -0,0 +1,30 @@
+
+import java.util.Scanner;
+
+public class Palindrome1 {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Enter the String : ");
+        String str = scan.next();
+
+        char charArr[] = str.toCharArray();
+        char revArr[] = new char[charArr.length];
+
+        int j = revArr.length - 1;
+
+        for (int i = 0; i <= charArr.length - 1; i++) {
+            revArr[j] = charArr[i];
+            j--;
+        }
+
+        String revStr = new String(revArr);
+
+        if (str.equalsIgnoreCase(revStr)) {
+            System.out.println("Given Strings are Palindrome");
+        } else {
+            System.out.println("Given Strings are NOT Palindrome");
+        }
+    }
+}
